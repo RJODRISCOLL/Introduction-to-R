a776954f9707248b381795815b4955e203899c20
RJODRISCOLL <48929734+RJODRISCOLL@users.noreply.github.com>
2020-01-23 11:23:33 +0000

    1.2

---
+++ 1.2 Introduction to RStudio and Packages/Solutions/1.2 Solutions.R
@@ -0,0 +1,29 @@
+#1. Use a # to comment a description of your script
+#2. Install and load the 'dplyr' package 
+#3. Print the statement 'i have installed dplyr'
+#4. Get help on the dplyr function 'mutate', 'filter' and 'arrange' 
+#5. Save the script
+
+#1. Use a # to comment a description of your script
+
+# I am attempting the additional resourses of the intro to R workshop 
+
+
+#2. Install and load the 'dplyr' package 
+
+install.packages('dplyr')
+library(dplyr)
+
+#3. Print the statement 'i have installed dplyr'
+
+print('i have installed dplyr')
+
+#4. Get help on the dplyr function 'mutate', 'filter' and 'arrange' 
+
+help("mutate")
+help("filter")
+help("arrange")
+
+#5. Save the script
+
+# file > save as 
